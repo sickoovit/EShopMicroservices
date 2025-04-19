c3915d398046c757e4545e033d2ccb56f0a78e07
sickoovit <omarnasser665@gmail.com>
2025-04-19 22:08:01 +0200

    implement additional abstractions for cqrs

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,10 @@
+﻿namespace BuildingBlocks.CQRS;
+
+public interface ICommand : ICommand<Unit>
+{
+
+}
+
+public interface ICommand<out TResponse> : IRequest<TResponse>
+{
+}
---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQuery.cs
@@ -0,0 +1,6 @@
+﻿namespace BuildingBlocks.CQRS;
+
+public interface IQuery<out TResponse> : IRequest<TResponse>
+    where TResponse : notnull
+{
+}
