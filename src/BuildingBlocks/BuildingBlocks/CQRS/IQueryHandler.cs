b4d37e3708f41b0c8f33dfc4bff158a8451163ab
sickoovit <omarnasser665@gmail.com>
2025-04-22 00:29:55 +0200

    create mediatR handler abstractions

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/IQueryHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace BuildingBlocks.CQRS;
+
+public interface IQueryHandler<in TQuery, TResponse>
+    : IRequestHandler<TQuery, TResponse>
+    where TQuery : IQuery<TResponse>
+    where TResponse : notnull
+{
+}
