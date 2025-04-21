53b136703e8ea98a58e64aa20b3f2c48e52e229e
sickoovit <omarnasser665@gmail.com>
2025-04-22 00:30:17 +0200

    create carter extension method to specify assemblies

---
+++ src/BuildingBlocks/BuildingBlocks/Extensions/CarterExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace BuildingBlocks.Extensions;
+public static class CarterExtentions
+{
+    public static IServiceCollection AddCarterWithAssemblies
+        (this IServiceCollection services, params Assembly[] assemblies)
+    {
+        services.AddCarter(configurator: config =>
+        {
+            foreach (var assembly in assemblies)
+            {
+                var modules = assembly.GetTypes()
+                .Where(t => t.IsAssignableTo(typeof(ICarterModule))).ToArray();
+
+                config.WithModules(modules);
+            }
+        });
+
+        return services;
+    }
+}
